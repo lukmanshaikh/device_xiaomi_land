7816fe69766f2678441bb1c4c330ba9ae73671b0
Lukman Shaikh <shaikhlukman01@gmail.com>
2018-09-12 03:38:48 +0000

    Specify LOCAL_PRIVATE_PLATFORM_APIS

--- XiaomiParts/Android.mk
+++ XiaomiParts/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiParts
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_AAPT_FLAGS := --auto-add-overlay
@@ -23,3 +24,4 @@ LOCAL_DEX_PREOPT := false
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
